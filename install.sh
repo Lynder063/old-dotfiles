bcdd8916f9151e17d14541a2718087c6a710c3a5
Lynder063 <lynderjekokot@gmail.com>
2024-02-07 15:43:54 +0100

    Update

---
+++ install.sh
@@ -0,0 +1,6 @@
+#/bin/bash/
+yay -S kitty grim wofi waybar neovim ttf-font-awesome noto-fonts-enoji \
+thunar thunar-volman gvfs network-manager-applet dunst hyprpaper \
+swaylock-effects ranger flameshot-gui ttf-hack-nerd
+&&  
+git clone https://github.com/Lynder063/dotfile.git ~/.config/
