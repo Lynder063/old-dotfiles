8cf12a6e464c1697f065f95a026273e53e66d5c2
Lynder063 <lynderjekokot@gmail.com>
2024-02-10 15:24:06 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #/bin/bash/
-yay -S kitty grim wofi waybar neovim ttf-font-awesome noto-fonts-enoji \
-thunar thunar-volman gvfs network-manager-applet dunst hyprpaper \
-swaylock-effects ranger flameshot-gui ttf-hack-nerd
-&&  
-git clone https://github.com/Lynder063/dotfile.git ~/.config/
+yay -S kitty grim wofi waybar neovim ttf-font-awesome noto-fonts-enoji thunar thunar-volman gvfs network-manager-applet dunst hyprpaper swaylock-effects ranger flameshot-gui ttf-hack-nerd &&  
+
+# Git cloning repository to .config folder
+# Can make problems when already created .config folder
+# Usually i run rm -r .config but i dont recommend that
+  git clone https://github.com/Lynder063/dotfile.git ~/.config/
