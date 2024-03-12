f8b529c3e7cfecbccc7e220e8f44791332ace271
Lynder063 <lynderjekokot@gmail.com>
2024-03-12 08:14:56 +0100

    Typos in script

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #/bin/bash/
-yay -S kitty grim wofi waybar neovim ttf-font-awesome noto-fonts-enoji thunar thunar-volman gvfs network-manager-applet dunst hyprpaper swaylock-effects ranger flameshot-gui ttf-hack-nerd &&  
+yay -S hyprland kitty grim wofi waybar neovim ttf-font-awesome noto-fonts-emoji thunar thunar-volman gvfs network-manager-applet dunst hyprpaper swaylock-effects ranger ttf-hack-nerd catppuccin-gtk-theme-mocha &&  
 
-# Git cloning repository to .config folder
-# Can make problems when already created .config folder
-# Usually i run rm -r .config but i dont recommend that
-  git clone https://github.com/Lynder063/dotfile.git ~/.config/
-
-
-catppuccin-gtk-theme-mocha
+  echo "All packages installed :)"  
