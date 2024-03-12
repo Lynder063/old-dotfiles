97187255509b462397fba3c7785dafa58b1d5f50
Lynder063 <lynderjekokot@gmail.com>
2024-03-12 08:04:11 +0100

    Update

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ yay -S kitty grim wofi waybar neovim ttf-font-awesome noto-fonts-enoji thunar th
 # Can make problems when already created .config folder
 # Usually i run rm -r .config but i dont recommend that
   git clone https://github.com/Lynder063/dotfile.git ~/.config/
+
+
+catppuccin-gtk-theme-mocha
