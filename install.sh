dc1149e3ec670d4c474bf91e81532eb3e0b259c7
Lynder063 <lynderjekokot@gmail.com>
2024-03-13 09:46:33 +0100

    Edited install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #/bin/bash/
 yay -S hyprland kitty grim wofi waybar neovim ttf-font-awesome noto-fonts-emoji thunar thunar-volman gvfs network-manager-applet dunst hyprpaper swaylock-effects ranger ttf-hack-nerd catppuccin-gtk-theme-mocha &&  
-
-  echo "All packages installed :)"  
+echo "All packages installed :)"
+chmod +x $HOME/.config/wofi/screenshot.sh &&
+sudo cp $HOME/.config/wofi/screenshot.sh /usr/local/bin/
