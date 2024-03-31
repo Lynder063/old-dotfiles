aeb18ef5762011ec02cdf8c79939611767471496
Lynder063 <lynderjekokot@gmail.com>
2024-03-31 16:25:24 +0200

    Wrong CPU usage on Waybar

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,9 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
+vim.g.vimtex_view_method = 'zathura'
+
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
@@ -25,10 +28,15 @@ require("lazy").setup({
     end,
   },
 
+{ 
+    'IogaMaster/neocord',
+    event = "VeryLazy"
+},
+
 {
   "lervag/vimtex",
-  lazy = false, -- lazy-loading will disable inverse search
-},
+  lazy = false,
+  },
 
   { import = "plugins" },
 }, lazy_config)
